76d8869b76caecfe24f4f11786347124087c762e
Luiz Fernando Meira Filho <fmeira.filho@gmail.com>
2021-07-20 22:04:59 -0600

    created new customType for Number/Float type.. ts sucks with this

--- src/entities/Token.ts
+++ src/entities/Token.ts
@@ -1,4 +1,26 @@
-import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import {
+  Entity,
+  EntityProperty,
+  Platform,
+  PrimaryKey,
+  Property,
+  Type,
+} from "@mikro-orm/core";
+// import Decimal from "decimal.js";
+
+class DecimalType extends Type<number, string> {
+  convertToDatabaseValue(value: number, _platform: Platform): string {
+    return value.toString();
+  }
+
+  convertToJSValue(value: any, _platform: Platform): any {
+    return new Number(value);
+  }
+
+  getColumnType(_prop: EntityProperty, _platform: Platform): string {
+    return `double precision`;
+  }
+}
 
 @Entity()
 export class Token {
@@ -20,6 +42,6 @@ export class Token {
   @Property({ type: "date", onUpdate: () => new Date() })
   updatedAt = new Date();
 
-  @Property({ default: 0 })
-  liquidation_ratio_maker!: number;
+  @Property({ default: 0, type: DecimalType })
+  liquidation_ratio_maker!: number; //it's easier to store as string and convert after, damn TS/JS non float support..
 }
