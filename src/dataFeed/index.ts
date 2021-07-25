322e9af04d99ea2c0efd0681ccce5166a5752170
Luiz Fernando Meira Filho <fmeira.filho@gmail.com>
2021-07-25 12:28:57 -0600

    added DAI checker before initial insertion

--- src/dataFeed/index.ts
+++ src/dataFeed/index.ts
@@ -1,18 +1,26 @@
 import { MikroORM } from "@mikro-orm/core";
+import chalk from "chalk";
 import { Token } from "../entities/Token";
 import config from "../mikro-orm.config";
 import makerCrawler from "./maker/crawler";
 
 export default async () => {
-  // initial crucial insertions
   const orm = await MikroORM.init(config);
-  const dai = orm.em.create(Token, {
-    token_ERC_code: "DAI",
-    token_code_on_protocol: "DAI",
-    token_address: "0x6b175474e89094c44da98b954eedeac495271d0f",
-    decimals: 18,
-  });
-  orm.em.persistAndFlush(dai);
+
+  //// initial crucial insertions
+
+  // checking DAI
+  const daiSearch = await orm.em.findOne(Token, { token_ERC_code: "DAI" });
+  if (daiSearch === null) {
+    const dai = orm.em.create(Token, {
+      token_ERC_code: "DAI",
+      token_code_on_protocol: "DAI",
+      token_address: "0x6b175474e89094c44da98b954eedeac495271d0f",
+      decimals: 18,
+    });
+    orm.em.persistAndFlush(dai);
+    console.log(chalk.magentaBright(`Added new TOKEN to database: DAI}`));
+  }
 
   //calling crawlers
   await makerCrawler();
